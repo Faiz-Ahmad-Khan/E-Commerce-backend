95b39cdf674ce82c223dd0614c696d72959e3856
Faiz-Ahmad-Khan <faizahmadkhan999@gmail.com>
2023-03-26 17:38:50 -0700

    c

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000", "https://e-commerce.onrender.com"]
+    origin:["http://localhost:3000"]
 }));
 
 app.post("/register", async (req, resp) => {
