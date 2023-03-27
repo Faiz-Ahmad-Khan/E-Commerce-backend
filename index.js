ca216c8f395f2cbfabad2826a8fddb924b06b6ab
Faiz-Ahmad-Khan <faizahmadkhan999@gmail.com>
2023-03-27 14:17:37 -0700

    c

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ const jwtKey = 'e-comm';
 const app = express();
 
 app.use(express.json());
--app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://e-commerce-nmdx.onrender.com"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://e-commerce-nmdx.onrender.com"],
+//   })
+// );
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post("/register", async (req, resp) => {
     let user = new User(req.body);
