c2a7effe6e3aea9cbebe79a4fd85f9fea04cf1fe
Faiz-Ahmad-Khan <faizahmadkhan999@gmail.com>
2023-03-26 17:35:09 -0700

    .env added

--- db/config.js
+++ db/config.js
@@ -1,3 +1,9 @@
 const mongoose= require('mongoose');
 mongoose.set('strictQuery',false);
-mongoose.connect('mongodb://127.0.0.1/e-commerce',{useNewUrlParser: true});
+mongoose.connect(
+    process.env.MONGO_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
