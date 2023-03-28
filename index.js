d12bcbaf97af1312fd8c701dd41f7f52da95cc2f
Faiz-Ahmad-Khan <faizahmadkhan999@gmail.com>
2023-03-28 06:38:37 -0700

    c

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://e-commerce-nmdx.onrender.com"],
+    origin: ["http://localhost:3000", "https://e-commerce.onrender.com"],
   })
 );
 
